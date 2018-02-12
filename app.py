58f51a7b8f7e522a27003a1cb95f7b0683e8b969
Quidge <trydemirgian@gmail.com>
2018-02-11 19:03:02 -0500

    Wrote helper functions. Added a gitignore for __pycache__/ that's popping up.

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-from helpers import getJSON
+import json
 
-api_key = "Jonathan-testapp-SBX-dbffbb2e5-006125e7"
-endpoint = "http://svcs.sandbox.ebay.com/services/search/FindingService/v1"
+from helpers import findItemsByKeywords, findItemsAdvanced, findItemsBySeller
+
+endpoint = "http://svcs.ebay.com/services/search/FindingService/v1"
+api_key = "***REMOVED***"
+
+limit10 = {
+	"paginationInput.entriesPerPage": 5,
+}
+
+resp = findItemsBySeller(endpoint, api_key, 'balearic1', keywords=limit10)
+
+print(json.dumps(json.loads(resp.read()), indent=4))
--- helpers.py
+++ helpers.py
@@ -2,10 +2,10 @@ import json
 
 from urllib import request, parse
 
-#endpoint = "http://svcs.sandbox.ebay.com/services/search/FindingService/v1"
-#api_key = "Jonathan-testapp-SBX-dbffbb2e5-006125e7"
+endpoint = "http://svcs.ebay.com/services/search/FindingService/v1"
+api_key = "***REMOVED***"
 
-def getJSON(endpoint, API_KEY, keywords):
+def findItemsByKeywords(endpoint, API_KEY, keywords, additionalVals=None):
 	"""Takes an endpoint and a dict holding name=value pairs. Returns a JSON objectwith results.
 
 	search() will do the URL encoding for you. 
@@ -20,16 +20,33 @@ def getJSON(endpoint, API_KEY, keywords):
 	})
 
 	url = endpoint + "?" + defaults + "&REST-PAYLOAD&" + parse.urlencode(keywords, quote_via=parse.quote)
-	print(url+"\n")
+	if additionalVals:
+		url += "&" + parse.urlencode(additionalVals, quote_via=parse.quote)
 
-	resp = request.urlopen(url)
-
-	#print(resp.status)
-	return parsed = json.loads(resp.read())
-	#print(json.dumps(parsed, indent=4))
-	#print(f.reason)
-	#repr(f)
-
+	return request.urlopen(url)
 	
+def findItemsAdvanced(endpoint, API_KEY, keywords):
+	"""Takes an endpoint and a dict holding name=value pairs. Returns a JSON objectwith results.
 
-#getJSON(endpoint, api_key, {"keywords": "harry potter"})
+	search() will do the URL encoding for you. 
+	"""
+
+	defaults = parse.urlencode({
+		"OPERATION-NAME": "findItemsAdvanced",
+		"SERVICE-VERSION": "1.0.0",
+		"SECURITY-APPNAME": api_key,
+		"GLOBAL-ID": "EBAY-US",
+		"RESPONSE-DATA-FORMAT": "JSON"
+	})
+
+	url = endpoint + "?" + defaults + "&REST-PAYLOAD&" + parse.urlencode(keywords, quote_via=parse.quote)
+
+	return request.urlopen(url)
+
+def findItemsBySeller(endpoint, API_KEY, sellerName, keywords={}):
+	"""Helper function that wraps findItemsAdvanced and adds sellerName as key"""
+	
+	keywords["itemFilter.name"] = "Seller"
+	keywords["itemFilter.value"] = sellerName
+
+	return findItemsAdvanced(endpoint, API_KEY, keywords)
