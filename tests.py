c2414bf4855332d8df58792e478cd1c1d3b2e070
Quidge <trydemirgian@gmail.com>
2018-05-14 09:50:07 -0400

    Fixed untracked files. Stopped tracking test.db

--- tests.py
+++ tests.py
@@ -1,8 +1,9 @@
 import unittest
 import os
+import sqlalchemy
 
 from app import app, db
-from app.models import User
+from app.models import User, SizeKeyShirtCasual, SizeKeyShirtDressNeck, SizeKeyShirtDressSleeve
 from app.dbtouch import get_user_sizes_subscribed
 
 
@@ -24,11 +25,47 @@ class TestCase(unittest.TestCase):
 		db_should_have = set(['shirt-sleeve', 'shirt-neck', 'shirt-casual'])
 		self.assertEqual(db_has, db_should_have)
 
-	def test_duplicate_size_association_to_user(self):
-		# This should assert that it is not possible to associate a size with a user
-		# twice. This should test every size table.
-		pass
+	# I can't get the following test to run properly.
+	'''def test_duplicate_size_association_to_user(self):
+		# This should assert that it is not possible to associate the same size with the same
+		# user twice (testing for uniqueness). This should test every size relationship.
 
+		# Create a user
+		db.session.add(User(email='duplicate_associations@test.com', password='password'))
+		db.session.flush()
+		u = User.query.filter(User.email == "duplicate_associations@test.com").first()
+
+		# Grab instances of each size category
+		test_shirt_neck = SizeKeyShirtDressNeck.query.first()
+		test_shirt_casual = SizeKeyShirtCasual.query.first()
+		test_shirt_sleeve = SizeKeyShirtDressSleeve.query.first()
+
+		# This will set a base state that will be rolled back to on each attempt
+		u.all_sizes_in_dict()
+		u.sizes['shirt-sleeve']['relationship'].append(test_shirt_sleeve)
+		u.sizes['shirt-neck']['relationship'].append(test_shirt_neck)
+		u.sizes['shirt-casual']['relationship'].append(test_shirt_casual)
+		db.session.commit()
+
+		# Refresh the sizes
+		u.all_sizes_in_dict()
+
+		with self.assertRaises(sqlalchemy.exc.IntegrityError):
+			u.sizes['shirt-sleeve']['relationship'].append(test_shirt_sleeve)
+			db.session.flush()
+			db.session.rollback()
+			#print('ran1')
+		with self.assertRaises(sqlalchemy.exc.IntegrityError):
+			u.sizes['shirt-neck']['relationship'].append(test_shirt_neck)
+			db.session.flush()
+			db.session.rollback()
+			#print('ran2')
+		with self.assertRaises(sqlalchemy.exc.IntegrityError):
+			u.sizes['shirt-casual']['relationship'].append(test_shirt_casual)
+			db.session.flush()
+			db.session.rollback()
+			#print('ran3')
+	'''
 
 def main():
 	unittest.main()
