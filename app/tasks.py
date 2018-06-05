254e9ed3ae4acb174e28c39417bb50aaacb91804
Quidge <trydemirgian@gmail.com>
2018-06-05 14:01:41 -0400

    Tied together a query for items > create models task/function. Turns out SQLite hates concurrency.

--- app/tasks.py
+++ app/tasks.py
@@ -1,9 +1,11 @@
 import os
 from json import loads, dumps
+from time import sleep
 
 from celery import Celery, group, chain
+from celery.utils.log import get_task_logger
 #from .ebay_api_scripts import f_api
-from ebaysdk.exception import ConnectionError
+from ebaysdk.exception import ConnectionError, ConnectionResponseError
 from ebaysdk.shopping import Connection as Shopping
 from ebaysdk.finding import Connection as Finding
 from sqlalchemy.orm.exc import NoResultFound
@@ -14,7 +16,9 @@ from .models import Item
 celery = Celery(
 	'{}'.format(__name__),
 	broker='amqp://localhost',
-	backend='rpc://')
+	backend='amqp://')
+
+logger = get_task_logger(__name__)
 
 prodEndpoint = "open.api.ebay.com"
 prodAppId = "***REMOVED***"
@@ -43,7 +47,8 @@ def create_item_model(
 	with_sizes=False,
 	affiliate_url=None):
 	"""Celery task wrapper for model_builders.build_ebay_item_model."""
-	api_response = loads(json_api_responsen)
+
+	api_response = loads(json_api_response)
 	try:
 		item = build_ebay_item_model(
 			api_response,
@@ -59,10 +64,14 @@ def create_item_model(
 
 
 @celery.task
-def lookup_single_item(ebay_item_id, html_description=None):
+def lookup_single_item(ebay_item_id, html_description=False):
 	"""Task to lookup up a single item. Asking for additional HTML
 	description information is option. Returns JSON.
 	"""
+	#logger = lookup_single_item.get_logger()
+	#logger.info = ("""
+	#	Running GetSingleItem API for ebay id <{}>. Description requested: <{}>
+	#	""".format(ebay_item_id, html_description))
 	payload = {'ItemID': ebay_item_id}
 	if html_description:
 		setattr(payload, 'IncludeSelector', 'Description')
@@ -73,8 +82,8 @@ def lookup_single_item(ebay_item_id, html_description=None):
 	return response.json()
 
 
-@celery.task
-def get_seller_items(seller_id, items_category=None):
+@celery.task(bind=True)
+def get_seller_items(self, seller_id, items_category=None):
 	"""Returns a JSON response of all items a <seller_id> has available. Uses
 	the findItemsAdvanced ebay API. If items_category is provided, that will be
 	used as an item filter in the payload.
@@ -85,12 +94,15 @@ def get_seller_items(seller_id, items_category=None):
 	"""
 	payload = {'itemFilter': {'name': 'Seller', 'value': seller_id}}
 	if items_category:
-		#setattr(payload, 'categoryId', items_category)
 		payload['categoryId'] = items_category
 	try:
 		response = f_api.execute('findItemsAdvanced', payload)
 	except ConnectionError:
 		raise
+
+	r_dict = response.dict()
+	if r_dict['ack'] != 'Success':
+		raise ConnectionResponseError(r_dict['errorMessage'], response=response)
 
 	return response.json()
 
@@ -113,24 +125,42 @@ def add_new_items_from_seller_to_db(
 	seller_ebay_id,
 	items_category=None,
 	html_description=False):
-
-	'''new_items = chain(
-		get_seller_items.s(seller_ebay_id, items_category),
-		get_list_of_items_not_present_in_db.s(),
-		lookup_multiple_items.s(html_description=html_description)()
-	)'''
+	"""Does not yet create Item models with results."""
 
 	new_items = chain(
 		get_seller_items.s(seller_ebay_id, items_category),
 		get_list_of_items_not_present_in_db.s()
 	)()  # Calling it turns to a lazy-load AsyncResult
 
-	new_items_obj = loads(new_items.get())  # Calling .get() on an AsynchResult waits for the actual value
+	# Calling .get() on an AsynchResult waits for the actual value (but is sync and blocking)
+	new_items_obj = loads(new_items.get())
 
-	new_items_detail = group(
-		[lookup_single_item.s(item_id, html_description) for item_id in new_items_obj])
+	#result_to_model_workflow = chain(lookup_single_item.s(ebay_item_id), create_item_model.s(ebay_seller_id=seller_ebay_id))
 
-	return new_items_detail()
+	#new_items_detail = group(
+	#	lookup_single_item.s(item_id, html_description) for item_id in new_items_obj)
+
+	lookup_and_create_models_job = []
+	for item_id in new_items_obj:
+		job = chain(
+			lookup_single_item.s(item_id, html_description=html_description),
+			create_item_model.s(ebay_seller_id=seller_ebay_id, with_measurements=html_description))
+		lookup_and_create_models_job.append(job)
+	lookup_and_create_models_job = group(lookup_and_create_models_job)
+
+	return lookup_and_create_models_job()
+
+
+def lookup_item_and_add(ebay_item_id):
+	workflow = chain(lookup_single_item.s(ebay_item_id), create_item_model.s(ebay_seller_id='balearic1'))
+	return workflow()
+
+
+@celery.task
+def add(x, y):
+	sleep(5)
+	#self.update_state(state='SUCCESS')
+	return x + y
 
 
 
