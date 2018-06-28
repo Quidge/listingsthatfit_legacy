bc713aacf13ab83e004380519a714ae419614505
Jonathan Demirgian <lirum@Jonathans-Air.attlocal.net>
2018-06-28 13:39:39 -0400

    Multiple filter()s were redundant.

--- app/instance/stupid_query.py
+++ app/instance/stupid_query.py
@@ -3,6 +3,10 @@ from app.models import Item, MeasurementType as Type, ItemMeasurementAssociation
 from app import db
 
 
+measurements_dict = {}
+# measurements_dict = 
+
+
 def search_for_matching_measurements(seller, clothing_category_list, category, attribute, msmt, tolerance):
 	s = db.session
 	msmt_range = [msmt - tolerance, msmt + tolerance]
@@ -10,20 +14,21 @@ def search_for_matching_measurements(seller, clothing_category_list, category, a
 		clothing_category_list, attribute, min(msmt_range), max(msmt_range)))
 	print('----------')
 
-	res = s.query(Assoc, Item, Type).\
-		filter(Item.ebay_primary_category.in_(clothing_category_list)).\
-		filter(Assoc.fk_measurement_id == Type.id).\
-		filter(Assoc.fk_ebay_items_id == Item.id).\
-		filter(Type.attribute == attribute).\
-		filter(Type.clothing_category == category).\
-		filter(Assoc.measurement_value >= min(msmt_range)).\
-		filter(Assoc.measurement_value <= max(msmt_range)).\
+	res = s.query(Assoc, Item, Type).filter(
+		Item.ebay_primary_category.in_(clothing_category_list),
+		Assoc.fk_measurement_id == Type.id,
+		Assoc.fk_ebay_items_id == Item.id,
+		Type.attribute == attribute,
+		Type.clothing_category == category,
+		Assoc.measurement_value >= min(msmt_range),
+		Assoc.measurement_value <= max(msmt_range)).\
 		all()
+
 	return res
 
 
 if __name__ == '__main__':
-	results = search_for_matching_measurements('baearic1', [3001, 3002], 'sportcoat', 'chest', 22000, 1000)
+	results = search_for_matching_measurements('baearic1', [3002], 'sportcoat', 'chest', 22000, 1000)
 	'''for t, a in results:
 		print(a.item.ebay_item_id, ' | ', a.item.ebay_title)
 		print(t.clothing_category, t.attribute, a.measurement_value)'''
