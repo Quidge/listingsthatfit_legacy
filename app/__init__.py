78bd71f6e4cdcda0576192fcfd9e2f830ad6f52b
Jonathan Demirgian <lirum@Jonathans-Air.attlocal.net>
2018-07-31 14:06:48 -0400

    configured simple logs

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from celery import Celery
+from config import Config
+
+from logging.handlers import RotatingFileHandler
+import os
+import logging
 
 app = Flask(__name__)
-app.config.from_envvar('DEV_CONFIG_SETTINGS')
+# app.config.from_envvar('DEV_CONFIG_SETTINGS')
+app.config.from_object(Config)
 
 lm = LoginManager()
 lm.init_app(app)
@@ -20,3 +26,16 @@ from app import models, views
 	backend='ampq://',
 	include=['app.celery_tasks']
 )'''
+
+# logging
+if not app.debug:
+	if not os.path.exists('logs'):
+		os.mkdir('logs')
+	fh = RotatingFileHandler('logs/ltf.log', maxBytes=10240, backupCount=10)
+	fh.setFormatter(
+		logging.Formatter('%(asctime)s %(name)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+	fh.setLevel(logging.INFO)
+	app.logger.addHandler(fh)
+
+	app.logger.setLevel(logging.INFO)
+	app.logger.info('LTF startup')
--- config.py
+++ config.py
@@ -4,8 +4,16 @@ import os
 
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join('..', 'app.db')
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/ltf_db'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/ltf_db'
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 #SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = 'this one' # change to hash at some point
+# SQLALCHEMY_TRACK_MODIFICATIONS = False
+# SECRET_KEY = 'this one' # change to hash at some point
+
+class Config(object):
+	DEBUG = False
+	SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/ltf_db'
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'this one'
+	SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/ltf_db'
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+
